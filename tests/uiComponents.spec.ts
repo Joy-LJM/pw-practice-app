ec3b794859411220a87de366982618ab091bee8f
Joy-LJM <2522702541@qq.com>
2025-12-24 18:57:29 -0500

    test: web tables data update

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -147,3 +147,30 @@ test("window dialog box action confirm", async ({ page }) => {
     "mdo@gmail.com"
   );
 });
+test("web tables data update", async ({ page }) => {
+  await page.getByText("Tables & Data").click();
+  await page.getByText("Smart Table").click();
+
+  // 1. get the row by any test in this row
+  const targetRow = page.getByRole("row", {
+    name: "twitter@outlook.com",
+  });
+  await targetRow.locator(".nb-edit").click();
+  await page.locator("input-editor").getByPlaceholder("Age").clear();
+  await page.locator("input-editor").getByPlaceholder("Age").fill("25");
+  await page.locator(".nb-checkmark").click();
+
+  // 2. get the row based on the value in the specific column while same values existing in different rows
+  await page.locator(".ng2-smart-pagination-nav").getByText("2").click();
+  const targetRowById = page
+    .getByRole("row", { name: "11" })//2 rows have id 11 in the second column
+    .filter({ has: page.locator("td").nth(1).getByText("11") }); //get id 11 in the second column
+  await targetRowById.locator(".nb-edit").click();
+  await page.locator("input-editor").getByPlaceholder("E-mail").clear();
+  await page
+    .locator("input-editor")
+    .getByPlaceholder("E-mail")
+    .fill("test@gmail.com");
+  await page.locator(".nb-checkmark").click();
+  await expect(targetRowById.locator("td").nth(5)).toHaveText("test@gmail.com");
+});
