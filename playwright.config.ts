452085975bb23b51da4606477c7d14b08bbfad78
Joy-LJM <2522702541@qq.com>
2026-01-01 22:53:23 -0500

    test: add base url

--- playwright.config.ts
+++ playwright.config.ts
@@ -34,7 +34,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    // baseURL: 'http://localhost:3000',
+    baseURL: 'http://localhost:4200/',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -43,6 +43,7 @@ export default defineConfig({
     // Default timeout for navigating to a page in milliseconds, defaults to 0
     // navigationTimeout:1000,
     // video:'retain-on-failure'
+    // should run npm run usePageObject-chrome command to view the video
     video:{
       mode:'on',
       size:{
